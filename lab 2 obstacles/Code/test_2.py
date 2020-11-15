83736223448f502d8c87e2b5059cef2f9f586e94
Dmitry Devitt <giga.flopsis@gmail.com>
2020-11-15 20:09:50 +0300

    update planner

--- lab 2 obstacles/Code/test_2.py
+++ lab 2 obstacles/Code/test_2.py
@@ -6,11 +6,13 @@ from geometry_tools import *
 
 rad = math.radians
 
-targer_point = np.array([10, 0, 1])
-speed = 0.01
+targer_point = np.array([0, 10, 1])
+speed = 0.5
 
-c_rep = 0.1
-r_field = 2.
+r_tolerance = 1.0
+
+c_rep = 1.2
+r_field = 3.
 
 drone_pose = []
 
@@ -81,16 +83,33 @@ if __name__=="__main__":
 
         drone_pose = flib.get_pos(clientID, Quadricopter)
 
+
+        # формируем вектор скорости в направлении целевой точки
+        pose = flib.get_pos(clientID, QuadricopterT)
+        goal_vec = targer_point - drone_pose
+        norm_goal_vec = np.linalg.norm(goal_vec)
+        goal_vec = goal_vec / norm_goal_vec
+        # print("goal_vec", goal_vec)
+        goal_vec *= speed
+
+        # формирование вектора отталкивания
+
+        # Получает позици близжайшего препятствия
         obst_pose, dist_to_obst = get_near_obst(drone_pose, obst_list)
         print("obst_pose", obst_pose, "dist_to_obst", dist_to_obst)
 
+        # rep_vec = np.array([0,0])
+        rep_vec = obst_pose - drone_pose
+        power = rep_force(dist_to_obst)
+        rep_vec = rep_vec * power
+        print("rep forces", rep_vec)
 
-        pose = flib.get_pos(clientID, QuadricopterT)
-        goal_vec = targer_point - pose
-        goal_vec *= speed
 
+        # публикация в сим
         err = sim.simxSetObjectPosition(clientID, QuadricopterT, -1,
-                                        (pose[0] + goal_vec[0], pose[1] + goal_vec[1], pose[2] + +goal_vec[2]),
+                                        (drone_pose[0] + goal_vec[0]-rep_vec[0],
+                                         drone_pose[1] + goal_vec[1]-rep_vec[1],
+                                         drone_pose[2] + +goal_vec[2]),
                                         sim.simx_opmode_blocking)
         err = sim.simxSetObjectOrientation(clientID, QuadricopterT, -1, (0, 0, rad(0)), sim.simx_opmode_blocking)
 
